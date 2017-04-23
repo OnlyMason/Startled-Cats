5b815ed9d2b9c2592908d7f279dd8a3b8052ae29
nosam23@gmail.com <nosam23@gmail.com>
2017-04-22 23:16:54 -0700

    Next image feature
    Added click to get next image instead of having to reopen another
    toolbar popup
    
    Bug fixes

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-This was a firefox extension that I made for fun, and because startled cats are funny.
+This was a firefox extension that I made for fun, and because cats are funny.  I'm also a dog person.
 
-It grabs the newest submissions from imgur.com's 'startledcats' subreddit gallery and shows a random one to you when you click the toolbar button.
+It grabs the newest submissions from imgur.com's 'startledcats' and 'catgifs' subreddit galleries and shows a random one to you when you click the toolbar button.
 
 I do not claim ownership of any pictures that it may retrieve.
--- popup/image_script.js
+++ popup/image_script.js
@@ -1,39 +1,70 @@
-var URL = "https://api.imgur.com/3/gallery/r/startledcats/";
+var URL = "https://api.imgur.com/3/gallery/r/startledcats+catgifs/";
 var ID = "4a2515bbf9e3304";
 
 function request() {
-  var req = new XMLHttpRequest();  
-  req.open("GET", URL, true); // true for asynchronous  
+	var req = new XMLHttpRequest();  
+	req.open("GET", URL, true); // true for asynchronous  
   
-  req.onreadystatechange = function () {
-    if (req.readyState == XMLHttpRequest.DONE && req.status == 200) {
-      process(req.responseText);
-    }
-  }    
+	req.onreadystatechange = function () {
+		if (req.readyState == XMLHttpRequest.DONE && req.status == 200) {
+		  process(req.responseText);
+		}
+	}    
      
-  req.setRequestHeader('Authorization', 'Client-ID ' + ID);  
-  req.send(null);
+	req.setRequestHeader('Authorization', 'Client-ID ' + ID);  
+	req.send(null);
 }
 
 function process(response_text) {
-  console.log(response_text);
-  var images = JSON.parse(response_text);
-  var rand = Math.floor(Math.random() * (images.data.length));
-  var imageURL = "http://i.imgur.com/" + images.data[rand].id + ".jpg";
-  console.log(imageURL);
-  setPic(imageURL);
+	var images = JSON.parse(response_text);
+	var rand = Math.floor(Math.random() * (images.data.length));	
+	
+	// can't use albums
+	if (images.data[rand].is_album) {
+		var foundNew = false;
+		while (!foundNew) {
+			rand = Math.floor(Math.random() * (images.data.length));
+			if (!images.data[rand].is_album)
+				foundNew = true;
+		}
+	}
+	
+	if(images.data[rand].type == "image/gif") {
+		var imageURL = images.data[rand].mp4;
+		setVideo(imageURL);
+	}
+	else {
+		var imageURL = images.data[rand].link;
+		setPic(imageURL);
+	}
 }
 
 function setPic(addr) {
-  var catpic = document.getElementById("catPic");
-  catpic.src = addr;
+	removeEverything();
+	var catPic = document.createElement("img");
+	catPic.setAttribute("src", addr);
+	document.body.appendChild(catPic);
 }  
 
-// Doesn't work properly
-/* 
+
+function setVideo(addr) {
+	removeEverything();
+	var catVid = document.createElement("video");
+	catVid.src = addr;
+	catVid.type = "video/mp4";
+	catVid.autoplay = true;
+	catVid.loop = true;
+	document.body.appendChild(catVid);
+}
+
+function removeEverything() {
+	while (document.body.firstChild) {
+		document.body.firstChild.remove();
+	}
+}
+
 document.addEventListener("click", function() {
 	request();
-	console.log("CLICKED");
-}); */
+});
 
 request();
